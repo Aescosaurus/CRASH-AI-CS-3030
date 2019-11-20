ad7a6cbfd04221d0b21756b25941c553ab1aeac8
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-20 10:32:01 -0700

    Start button clickable.

--- game_interaction.py
+++ game_interaction.py
@@ -2,6 +2,7 @@ import win32gui
 import win32api
 import win32con
 from rect import rect_t
+import time
 
 def get_window_info():
     details = rect_t( 0,0,0,0 )
@@ -22,8 +23,15 @@ def get_window_info():
     return( details )
 
 def click_at( x,y ):
-    win32api.SetCursorPos( x,y )
+    win32api.SetCursorPos( ( x,y ) )
     win32api.mouse_event( win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0 )
     win32api.mouse_event( win32con.MOUSEEVENTF_LEFTUP,x,y,0,0 )
 
+def click_start():
+    window_rect = get_window_info()
+    click_at( window_rect.left + 215,window_rect.top + 520 )
+
 print( get_window_info() )
+time.sleep( 5 )
+click_start()
+print( "done" )
