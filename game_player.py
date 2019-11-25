a12cc37dafedcc5ffdea23a64d2d5a2b7db31eb9
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-25 10:31:06 -0700

    Trying to fix clicking/window rect problems.

--- game_player.py
+++ game_player.py
@@ -1,6 +1,5 @@
 import game_interaction as game
 import time
-from vec2 import vec2_t
 
 window_name = "Cave Runner Actual Sharp Hustle"
 
@@ -8,9 +7,7 @@ def start():
     game.focus_window( window_name )
     time.sleep( 0.2 )
     game.click_start()
-    pos = vec2_t( 0,350 ) # 8,352
-    print( game.get_pixel( pos.x,pos.y ) )
-    game.click_at( pos.x,pos.y )
+    # game.click_at( 0,0 )
 
 start()
 
