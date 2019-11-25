ab17461592f1f5df8dff1a42e0d18e78e7f752ac
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-25 09:37:58 -0700

    Clicking fixed and added key pressing.

--- game_interaction.py
+++ game_interaction.py
@@ -3,6 +3,15 @@ import win32api
 import win32con
 from rect import rect_t
 import time
+from pynput.keyboard import Controller
+import threading
+
+def focus_window( name ):
+    def check_window_enums( hWnd,name ):
+        if name == str( win32gui.GetWindowText( hWnd ) ):
+            win32gui.SetForegroundWindow( hWnd )
+
+    win32gui.EnumWindows( check_window_enums,name )
 
 def get_window_info():
     details = rect_t( 0,0,0,0 )
@@ -25,13 +34,20 @@ def get_window_info():
 def click_at( x,y ):
     win32api.SetCursorPos( ( x,y ) )
     win32api.mouse_event( win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0 )
+    time.sleep( 1.0 / 60.0 ) # This makes the buttons actually work.
     win32api.mouse_event( win32con.MOUSEEVENTF_LEFTUP,x,y,0,0 )
 
 def click_start():
     window_rect = get_window_info()
-    click_at( window_rect.left + 215,window_rect.top + 520 )
+    click_at( window_rect.left + 200,window_rect.top + 400 )
 
-print( get_window_info() )
-time.sleep( 5 )
+def press_key( key ):
+    press_key.kbd = Controller()
+    press_key.kbd.press( key )
+    press_key.kbd.release( key )
+
+window_rect = get_window_info()
+focus_window( "Cave Runner Actual Sharp Hustle" )
+time.sleep( 1 )
 click_start()
 print( "done" )
