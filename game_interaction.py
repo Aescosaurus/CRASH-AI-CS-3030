2d6ece465a6c8951ba1b1d01f107eea5121b904a
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-25 10:08:42 -0700

    Get color of pixels of game.

--- game_interaction.py
+++ game_interaction.py
@@ -4,6 +4,8 @@ import win32con
 from rect import rect_t
 import time
 from pynput.keyboard import Controller
+from color import color_t
+from vec2 import vec2_t
 
 def focus_window( name ):
     def check_window_enums( hWnd,name ):
@@ -16,34 +18,53 @@ def get_window_info():
     details = rect_t( 0,0,0,0 )
     def callback( hWnd,extra ):
         rect = win32gui.GetWindowRect( hWnd )
-        x = rect[0]
-        y = rect[1]
-        w = rect[2] - x
-        h = rect[3] - y
         if win32gui.GetWindowText( hWnd ) == "Cave Runner Actual Sharp Hustle":
-            extra.left = x
-            extra.top = y
-            extra.right = x + w
-            extra.bot = y + h
+            extra.left = rect[0]
+            extra.top = rect[1]
+            extra.right = rect[2]
+            extra.bot = rect[3]
 
     win32gui.EnumWindows( callback,details )
 
     return( details )
 
+def get_pixel( x,y ):
+    new_pos = local_to_global( vec2_t( x,y ) )
+    x = new_pos.x
+    y = new_pos.y
+
+    desktop_window = win32gui.GetDesktopWindow()
+    window_dc = win32gui.GetWindowDC( desktop_window )
+    color = int( win32gui.GetPixel( window_dc,x,y ) )
+    return( color_t(
+        ( color & 0xFF ), # Red
+        ( ( color >> 8 ) & 0xFF ), # Green
+        ( ( color >> 16 ) & 0xFF ) ) ) # Blue
+
 def click_at( x,y ):
+    new_pos = local_to_global( vec2_t( x,y ) )
+    x = new_pos.x
+    y = new_pos.y
+
     win32api.SetCursorPos( ( x,y ) )
     win32api.mouse_event( win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0 )
     time.sleep( 1.0 / 60.0 ) # This makes the buttons actually work.
     win32api.mouse_event( win32con.MOUSEEVENTF_LEFTUP,x,y,0,0 )
 
 def click_start():
-    window_rect = get_window_info()
-    click_at( window_rect.left + 200,window_rect.top + 400 )
+    click_at( 200,400 )
 
 def press_key( key ):
     press_key.kbd = Controller()
+
     press_key.kbd.press( key )
     press_key.kbd.release( key )
+
+def local_to_global( vec2 ):
+    local_to_global.window_rect = get_window_info()
+    x = vec2.x + local_to_global.window_rect.left
+    y = vec2.y + local_to_global.window_rect.top
+    return( vec2_t( x,y ) )
 
 """
 window_rect = get_window_info()
--- game_player.py
+++ game_player.py
@@ -1,10 +1,16 @@
 import game_interaction as game
 import time
+from vec2 import vec2_t
 
-window_name = game.get_window_info()
+window_name = "Cave Runner Actual Sharp Hustle"
 
 def start():
     game.focus_window( window_name )
+    time.sleep( 0.2 )
+    game.click_start()
+    pos = vec2_t( 0,350 ) # 8,352
+    print( game.get_pixel( pos.x,pos.y ) )
+    game.click_at( pos.x,pos.y )
 
 start()
 
