201126a58648cd009d1aa4cae7dcf7a42c2f11e4
rjstandifer <rjstandifer@comcast.net>
2019-12-15 15:07:22 -0700

    Update AiGraph.py

--- AiGraph.py
+++ AiGraph.py
@@ -15,8 +15,6 @@ import matplotlib.pyplot as plt
 alpha_data = []
 float_alpha_data = []
 
-delta_data = []
-float_delta_data = []
 
 # Read ai alpha test data from txt file
 
@@ -44,37 +42,10 @@ for item in alpha_data:
     
     float_alpha_data.append(float_item)
 
-# Read ai delta test data from txt file
-
-with open('AiDelta1000RandomTimes.txt', 'r') as file:
-    for line in file:
-
-        # Remove linebreaks
-
-        currentItem = line[:-1]
-
-        # Add item to list
-
-        delta_data.append(currentItem)
-
-
-# Close test data file
-
-file.close()
-
-# Cast list of strings to list of floats
-
-for item in delta_data:
-    
-    float_item = float(item)
-    
-    float_delta_data.append(float_item)
-
 
 # Graph the test data
 
 plt.plot(float_alpha_data, 'oy', label = 'alpha')
-plt.plot(float_delta_data, 'or', label = 'delta')
 plt.xlabel('AI Trial Runs')
 plt.ylabel('Seconds')
 plt.legend()
